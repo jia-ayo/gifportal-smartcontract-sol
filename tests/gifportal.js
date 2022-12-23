70ffea15ee8efa1f8f43c5715f59aa23000be1a2
jia-ayo <milkjia11@gmail.com>
2022-12-23 11:37:23 +0100

    baseaccount works

--- tests/gifportal.js
+++ tests/gifportal.js
@@ -3,11 +3,28 @@ const anchor = require("@project-serum/anchor");
 
 const main = async() => {
  console.log("starting test .....");
+
  const provider = anchor.AnchorProvider.env();
  anchor.setProvider(provider);
  const program = anchor.workspace.Gifportal;
- const tx= await program.rpc.startStuffOff();
- console.log("your transaction signatur", tx)
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+ console.log("your transaction signatur", tx);
+ 
+ let account = await program.account.baseAccount.fetch(
+    baseAccount.publicKey
+ );
+ console.log("Gif Count", account.totalGifs.toString())
 }
 
 const runMain = async() => {
