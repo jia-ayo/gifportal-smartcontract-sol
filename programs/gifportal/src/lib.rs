87a556f4f9c89636205ae878198ecf28b752258a
jia-ayo <milkjia11@gmail.com>
2022-12-23 11:59:22 +0100

    add gif

--- programs/gifportal/src/lib.rs
+++ programs/gifportal/src/lib.rs
@@ -12,6 +12,11 @@ pub mod gifportal {
       base_account.total_gifs=0;
       Ok(())
     }
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+      let base_account= &mut ctx.accounts.base_account;
+      base_account.total_gifs += 1;
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -21,6 +26,11 @@ pub struct StartStuffOff<'info>{
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+#[derive(Accounts)]
+pub struct AddGif<'info>{
+  #[account(mut)]
+  base_account: Account<'info, BaseAccount>
 }
 
 #[account]
--- tests/gifportal.js
+++ tests/gifportal.js
@@ -24,7 +24,17 @@ const main = async() => {
  let account = await program.account.baseAccount.fetch(
     baseAccount.publicKey
  );
- console.log("Gif Count", account.totalGifs.toString())
+ console.log("Gif Count", account.totalGifs.toString());
+
+ await program.rpc.addGif({
+    accounts: {
+        baseAccount: baseAccount.publicKey,
+    }
+ })
+
+ account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+ console.log("Gif Count", account.totalGifs.toString());
+
 }
 
 const runMain = async() => {
